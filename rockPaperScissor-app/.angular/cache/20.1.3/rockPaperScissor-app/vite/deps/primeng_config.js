6dfcdeb313c86098891e14571b8677b969d0bc30
AkhilKuchipudi <142478633+AkhilKuchipudi@users.noreply.github.com>
2025-07-30 13:18:21 +0530

    [ui]

---
+++ rockPaperScissor-app/.angular/cache/20.1.3/rockPaperScissor-app/vite/deps/primeng_config.js
@@ -0,0 +1,18 @@
+import {
+  PRIME_NG_CONFIG,
+  PrimeNG,
+  ThemeProvider,
+  providePrimeNG
+} from "./chunk-JWBXZTKE.js";
+import "./chunk-TBOAL3UM.js";
+import "./chunk-ZWA7AB7C.js";
+import "./chunk-JJOBUZZ2.js";
+import "./chunk-E3XXMAD7.js";
+import "./chunk-WJLPES6B.js";
+import "./chunk-GOMI4DH3.js";
+export {
+  PRIME_NG_CONFIG,
+  PrimeNG,
+  ThemeProvider,
+  providePrimeNG
+};
